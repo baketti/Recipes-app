f5c9f09982b651e32b49abbf44c3dc38a66ec3f7
emanuele-g-bachetti <e.bachetti1997@gmail.com>
2024-05-23 10:22:26 +0200

    fixed next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+// Ottieni l'URL del modulo corrente
+const __filename = fileURLToPath(import.meta.url);
+
+// Ottieni il percorso della directory corrente
+const __dirname = dirname(__filename);
+
+
 const nextConfig = {
     images: {
       remotePatterns: [
@@ -17,6 +27,10 @@ const nextConfig = {
     ],
     },
     transpilePackages: ['@mui/x-charts'],
+    webpack: (config) => {
+      config.resolve.alias['@'] = resolve(__dirname, './src');
+      return config;
+    },
 };
 
 export default nextConfig;
